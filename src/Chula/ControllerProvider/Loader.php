6e74e70a0bedec9ee7bb2797976d11653a0391f9
stephcook22 <stephcook22@gmail.com>
2013-10-20 20:16:32 +0100

    Added markdown to html converter
    Also added twig page template

--- src/Chula/ControllerProvider/Loader.php
+++ src/Chula/ControllerProvider/Loader.php
@@ -1,27 +1,29 @@
 <?php
+
 namespace Chula\ControllerProvider;
+
 use Silex\Application;
 use Silex\ControllerProviderInterface;
-use Symfony\Component\HttpFoundation\Request;
+use \Michelf\Markdown;
 
 class Loader implements ControllerProviderInterface {
 
-	public function connect(Application $app) {
-		$controllers = $app['controllers_factory'];
+    public function connect(Application $app) {
+	$controllers = $app['controllers_factory'];
 
 
-		$controllers->get('/', function($page) use ($app) {
-			$filepath = $app['config']['content_location'] . $page;
-			if(file_exists($filepath))
-			{
-				// TODO: do some fancy stuff that formats this all nicely
-				return file_get_contents($filepath);
-			}
-			else
-				$app->abort(404, "Those monkeys couldn't find the page you were after, hard luck.");
+	$controllers->get('/', function($page) use ($app) {
+		    $filepath = $app['config']['content_location'] . $page;
+		    if (file_exists($filepath)) {
+			$html = Markdown::defaultTransform(file_get_contents($filepath));
+			// TODO: do some fancy stuff that formats this all nicely
+			return $app['twig']->render('page.twig', array('content' => $html));
+		    }
+		    else
+			$app->abort(404, "Those monkeys couldn't find the page you were after, hard luck.");
 		});
 
-		return $controllers;
+	return $controllers;
+    }
 
-	}
 }
