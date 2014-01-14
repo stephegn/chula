9a1141586df19f5863d67e9ceae7c2e69fa0b5c5
Qas Ashraf <mail@qasashraf.co.uk>
2014-01-14 22:40:51 +0000

    fixed a potential bug, we now check if the content directory exists before enumerating it

--- src/Chula/ControllerProvider/Admin.php
+++ src/Chula/ControllerProvider/Admin.php
@@ -15,6 +15,10 @@ class Admin implements ControllerProviderInterface {
 	$controllers->get('/', function() use ($app) {
 	
 			//grab all items in our content dir
+			if(!file_exists($app['config']['content_location']))
+			{
+				$app->abort(500, "Those monkeys couldn't find the page you were after, hard luck.");
+			}
 		    $pages = scandir($app['config']['content_location']);
 			
 			//Remove the first two items in the array. These are always '.' and '..'
