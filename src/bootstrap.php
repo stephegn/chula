6622d39cfb2e3875155657cab3a8ac609121722a
stephcook22 <stephcook22@gmail.com>
2014-01-15 20:04:14 +0000

    Moved user details into config.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -32,10 +32,7 @@ $app['security.firewalls'] = array(
     'admin' => array(
         'pattern' => '^/'.$app['config']['admin_path'],
         'form' => array('login_path' => '/login', 'check_path' => '/'.$app['config']['admin_path'].'/login_check'),
-        'users' => array(
-            // raw password is foo
-            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-        ),
+        'users' => $app['config']['users'],
         'logout' => array('logout_path' => '/'.$app['config']['admin_path'].'/logout')
     ),
 );
