3eacc85d20391542f40d4a549cf5d3d8750cb286
stephcook22 <stephcook22@gmail.com>
2014-01-17 23:49:12 +0000

    Add check for content directory.
    Nothing will run if it doesn't exist.

--- src/app.php
+++ src/app.php
@@ -4,6 +4,9 @@ require_once __DIR__.'/bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
+if(!is_dir($app['config']['content_location'])) {
+	$app->abort(500, "There was an issue loading the content. Is your content location correct?");
+}
 $app->mount(
 		'/'.$app['config']['admin_path'],
 		new Chula\ControllerProvider\Admin()
