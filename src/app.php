5d81e38fceaef11f05a79d0ad5a163e2a7c9ffdf
stephcook22 <stephcook22@gmail.com>
2014-04-07 23:02:56 +0100

    Fixed admin kit 404ing

--- src/app.php
+++ src/app.php
@@ -8,6 +8,9 @@ foreach ($app['config']['location'] as $path)
     if (!is_dir($path)) {
   $app->abort(500, "There was an issue loading the content. Is your content location correct?");
 }
+
+$app->mount('/', new Chula\ControllerProvider\HomePage());
+
 $app->mount(
   '/' . $app['config']['admin_path'],
   new Chula\ControllerProvider\Admin()
@@ -38,7 +41,6 @@ $app->mount(
   new Chula\ControllerProvider\PublishDraft()
 );
 
-$app->mount('/', new Chula\ControllerProvider\HomePage());
 
 $app->get('/login', function(Request $request) use ($app) {
     return $app['twig']->render('admin_login.twig', array(
