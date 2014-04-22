b46c29a9c8f446d2ea552ad13f75f3cd9c452950
stephcook22 <stephcook22@gmail.com>
2014-04-22 22:37:13 +0100

    Created tests for new draft
    Added new test pages.
    Abstracted login
    Created test for new draft
    Added filesystem module to codeception acceptance config
    Added extra required commands to travis.yml
    Added extra ids to templates for testing

---
+++ tests/acceptance/NewPageCest.php
@@ -0,0 +1,32 @@
+<?php
+use \WebGuy;
+
+class NewPageCest
+{
+
+	public function _before()
+	{
+	}
+
+	public function _after()
+	{
+	}
+
+	// tests
+	public function createNewPage(WebGuy $I)
+	{
+
+		$I->am('Administrator');
+		$I->wantTo('create a new page');
+		$I->cleanDir('content/drafts');
+		AdminLoginPage::of($I)->login('admin', 'foo');
+		AdminHomePage::of($I)->clickNewPage();
+		NewPage::of($I)->createPage('test-draft', '#Testing');
+		$I->see('Drafts');
+		$I->seeElement('#drafts td');
+		$I->see('test-draft');
+		$I->seeFileFound('test-draft','content/drafts');
+	}
+
+
+}
---
+++ tests/acceptance/_pages/AdminLoginPage.php
@@ -0,0 +1,57 @@
+<?php
+
+class AdminLoginPage
+{
+	// include url of current page
+	static $URL = '/login';
+
+	/**
+	 * Declare UI map for this page here. CSS or XPath allowed.
+	 * public static $usernameField = '#username';
+	 * public static $formSubmitButton = "#mainForm input[type=submit]";
+	 */
+
+	static $usernameField = '.form-signin input[name=_username]';
+	static $passwordField = '.form-signin input[name=_password]';
+	static $loginButton = ".form-signin input[type=submit]";
+
+	/**
+	 * Basic route example for your current URL
+	 * You can append any additional parameter to URL
+	 * and use it in tests like: EditPage::route('/123-post');
+	 */
+	public static function route($param)
+	{
+		return static::$URL . $param;
+	}
+
+	/**
+	 * @var WebGuy;
+	 */
+	protected $webGuy;
+
+	public function __construct(WebGuy $I)
+	{
+		$this->webGuy = $I;
+	}
+
+	/**
+	 * @return UserLoginPage
+	 */
+	public static function of(WebGuy $I)
+	{
+		return new static($I);
+	}
+
+	public function login($name, $password)
+	{
+		$I = $this->webGuy;
+
+		$I->amOnPage(self::$URL);
+		$I->fillField(self::$usernameField, $name);
+		$I->fillField(self::$passwordField, $password);
+		$I->click(self::$loginButton);
+
+		return $this;
+	}
+}
