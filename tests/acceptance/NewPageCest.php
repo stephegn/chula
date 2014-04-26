602beb9b6b01ef2c71314ebd11d799cfa60ee031
stephcook22 <stephcook22@gmail.com>
2014-04-26 21:18:16 +0100

    Added test for adding new page when a page of that name already exists.

--- tests/acceptance/NewPageCest.php
+++ tests/acceptance/NewPageCest.php
@@ -6,11 +6,14 @@ class NewPageCest
 
 	public function _before()
 	{
+
 	}
 
 	public function _after()
 	{
+
 	}
+
 
 	// tests
 	public function createNewPage(WebGuy $I)
@@ -27,6 +30,19 @@ class NewPageCest
 		$I->see('test-draft');
 		$I->seeFileFound('test-draft','content/drafts');
 	}
+
+	public function createExistingPage(WebGuy $I)
+	{
+
+		$I->am('Administrator');
+		$I->wantTo('create a new page which already exists');
+		AdminLoginPage::of($I)->login('admin', 'foo');
+		AdminHomePage::of($I)->clickNewPage();
+		NewPage::of($I)->createPage('test-draft', '#Testing');
+		$I->see('That page already exists');
+		$I->cleanDir('content/drafts');
+	}
+
 
 
 }
