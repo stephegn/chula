bef6cee0be601ce2b93104a464ac428f4449a5f9
Qas Ashraf <mail@qasashraf.co.uk>
2014-04-29 00:34:33 +0100

    Started work on implementation of Filesystem implementation of a StorageInterface - delete function done. Not being used yet, plan is write the interface and test via unit tests prior to usage.

--- src/Chula/Service/StorageInterface.php
+++ src/Chula/Service/StorageInterface.php
@@ -11,6 +11,15 @@ namespace Chula\Service;
 
 interface StorageInterface
 {
+    public function create($name, $data);
 
+    public function delete($name);
 
+    public function update($name, $data);
+
+    public function read($name);
+
+    public function publish($name);
+
+    public function getPages($type);
 }
