37896f1033f3f339b78b9af4c7338ecd7eb897f2
Qas Ashraf <mail@qasashraf.co.uk>
2014-04-30 21:58:16 +0100

    Define how an implementation of a storage interface should behave via comments which describe the input and outputs. Needs to be reviewed before we can continue with the concrete implementation.

--- src/Chula/Service/StorageInterface.php
+++ src/Chula/Service/StorageInterface.php
@@ -12,15 +12,57 @@ use Chula\Model\Page;
 
 interface StorageInterface
 {
-    public function create($name, $data);
+    /**
+     * @param Page $page
+     *
+     * Consumes a \Chula\Model\Page object, handles the serialising of this object and it's storage.
+     *
+     */
+    public function create(Page $page);
 
-    public function delete(Page $name);
+    /**
+     * @param Page $page
+     *
+     * Consumes a \Chula\Model\Page object, purges the file from storage.
+     *
+     */
+    public function delete(Page $page);
 
-    public function update(Page $name, $data);
+    /**
+     * @param Page $old
+     * @param Page $new
+     *
+     * Takes two \Chula\Model\Page objects, replacing $old with $new. It disposes of the $old Page once it's done.
+     *
+     */
+    public function update(Page $old, Page $new);
 
-    public function read($name);
+    /**
+     * @param $type
+     * @param $name
+     *
+     * Finds a page representation and deserialising it, retuning a Page object to the sender.
+     *
+     * @return \Chula\Model\Page
+     */
+    public function read($type, $name);
 
-    public function publish($name);
+    /**
+     * @param Page $page
+     *
+     * Promotes a draft page to a published page, making changes on the storage provider as required.
+     *
+     */
+    public function publish(Page $page);
 
-    public function getPages($type);
+    /**
+     * @param $type
+     *
+     * Looks up all the available pages for a given $type, returns an array of \Chula\Model\Page objects. If no pages
+     * are found an empty array will be returned.
+     *
+     * @return mixed
+     */
+    public function getAllPages($type);
+
 }
